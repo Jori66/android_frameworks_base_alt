ce0f05797ea8487d5b97736eddfd6da6757dbdbc
Nicholas Semendyaev <agent00791@gmail.com>
2012-09-18 20:08:36 -0400

    Navigation bar custom height

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2749,6 +2749,12 @@ public final class Settings {
         public static final String APP_CIRCLE_BAR_SHOW_TRIGGER = "app_circle_bar_show_trigger";
 
         /**
+         * Height of navigation bar buttons
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
